b6a0395ff9b24aac2a219c260754ad4dd7a03fc7
Blackcobra660 <332496@student.mboutrecht.nl>
2023-06-21 09:44:21 +0200

    first commit

---
+++ app/Config/Config.php
@@ -0,0 +1,23 @@
+<?php
+    //Database parameters of mysql
+    
+    define('DB_HOST', 'localhost'); //Add your db host
+    define('DB_USER', 'root'); // Add your DB root
+    define('DB_PASS', ''); //Add your DB pass
+    define('DB_NAME', 'VoedselBank2'); //Add your DB Name
+    
+
+
+
+    //APPROOT
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    //URLROOT (Dynamic links)
+    define('URLROOT', 'http://voedselbank2.nl');
+
+    //Sitename
+    define('SITENAME', 'voedselbank2');
+
+    define('SQLROOT', '../app/Sqlscripts/');
+
+    
---
+++ app/Views/Includes/navigation.php
@@ -0,0 +1,12 @@
+<nav class="top-nav">
+    <ul>
+        <li>
+            <a href="<?= URLROOT; ?>/index">Home</a>
+        </li>
+        <!-- 
+            <li>
+                <a href="<?= URLROOT; ?>/about">About</a>
+            </li> 
+        -->
+    </ul>
+</nav>
